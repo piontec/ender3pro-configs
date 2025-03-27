1871d5ec4a9a6e5e18d42e9c7d46a6c12c7c6e26
ender <configs@ender>
2025-03-27 09:31:13 +0100

    New Backup on boot - 03/27/25 - 09:31:07

--- cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo logrotate /etc/logrotate.conf
-sudo rm /var/log/syslog.*
-sudo rm /var/log/daemon.log.*
-rm ~/.octoprint/logs/octoprint.log
-touch ~/.octoprint/logs/octoprint.log
